d1617485afec63f5b1844c27d936f95820962dd5
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-27 07:01:27 +0100

    Use the button to create the audioContext

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let audioContext;
+
 const startCtxBtn = document.querySelector(".start")
+
+// GETTING THE AUDIOCONTEXT AFTER THE USER INTERACTION - SO NO MORE SUPRISES
+startCtxBtn.addEventListener("click", ()=>{
+    audioContext = new AudioContext()
+})
