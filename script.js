fb7854512cebfc483b85b9c295d32012317b324b
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-27 07:31:57 +0100

    Load the Audios asynchronously

--- script.js
+++ script.js
@@ -2,7 +2,28 @@ let audioContext;
 
 const startCtxBtn = document.querySelector(".start")
 
-// GETTING THE AUDIOCONTEXT AFTER THE USER INTERACTION - SO NO MORE SUPRISES
+// GETTING THE AUDIOCONTEXT AFTER THE USER INTERACTION - SO NO MORE SUPRISES FROM THE BROWSER
+
 startCtxBtn.addEventListener("click", ()=>{
     audioContext = new AudioContext()
+    console.log("Audio Context started")
 })
+
+// IN ORDER TO PLAY ANYTHING, WE NEED THE AUDIO FILE(S)
+// YOU CAN LOAD FILES INDIVIDUALLY OR HAVE AN ARRAY OF FILES
+//
+// IN THIS TUTORIAL, WE WILL USE BUILT IN FETCH API TO GET OUR FILES - USE D API ONA FUNCTION
+
+// bcs fetch() api might take sm time we need to (a)wait for it - 
+// but await doesnt come on its own, 
+// it hase to be in async (more likely asyn fuctn y the functn has async). 
+// the response of the fetch() api is stored in the const response
+// the we turn the response into an array buffer - this requires sm await time too
+// then we make an audio buffer from the array buffer - this allows us to play the audio
+// lastly, we return the audioBuffer
+async function getFiles(filePath){
+   const response = await fetch(filePath);
+   const arrayBuffer = await response.arrayBuffer();
+   const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+   return audioBuffer;
+}
